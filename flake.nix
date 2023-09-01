1e3310ba9fe867e62ce18767e3022e1404a679c9
Adrian Hesketh <adrianhesketh@hushmail.com>
2023-09-01 18:46:51 +0100

    refactor: switch to buildNpmPackage

--- flake.nix
+++ flake.nix
@@ -1,30 +1,42 @@
 {
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/23.05";
-    npmlock2nix = {
-      url = "github:nix-community/npmlock2nix";
-      flake = false;
-    };
   };
 
-  outputs = { self, nixpkgs, npmlock2nix }:
+  outputs = { self, nixpkgs }:
     let
-      pkgs = nixpkgs.legacyPackages."x86_64-linux";
-      nl2nix = import npmlock2nix {inherit pkgs;};
-      app = nl2nix.v2.build {
-        src = ./.;
-        nodejs = pkgs.nodejs-18_x;
-        buildCommands = [ "HOME=$PWD" "npm run build" ];
-        installPhase = ''
-        mkdir -p $out
-          cp index.js $out/index.js
-          cp hello.sh $out/hello
-          chmod +x $out/hello
-        '';
-      };
+      # Systems supported
+      allSystems = [
+        "x86_64-linux" # 64-bit Intel/AMD Linux
+        "aarch64-linux" # 64-bit ARM Linux
+        "x86_64-darwin" # 64-bit Intel macOS
+        "aarch64-darwin" # 64-bit ARM macOS
+      ];
+
+      # Helper to provide system-specific attributes
+      forAllSystems = f: nixpkgs.lib.genAttrs allSystems (system: f {
+        pkgs = import nixpkgs { inherit system; };
+      });
     in
     {
-      packages."x86_64-linux".default = app;
+      packages = forAllSystems ({ pkgs }: {
+        default = pkgs.buildNpmPackage {
+          name = "buildNpmPackage-example";
+
+          buildInputs = with pkgs; [
+            nodejs-18_x
+          ];
+
+          src = ./.;
+          npmDepsHash = "sha256-o8ZcUbUhR2OH3gGRZwKtHfc8FllbG/QZ84T0Kivz8qM=";
+
+          installPhase = ''
+            mkdir -p $out
+            cp index.js $out/index.js
+            cp hello.sh $out/hello
+            chmod +x $out/hello
+          '';
+        };
+      });
     };
 }
-
